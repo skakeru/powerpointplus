7158c1d0628f78d3b676dd9fb45119088b713399
skakeru <49814523+skakeru@users.noreply.github.com>
2020-10-03 22:47:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,64 +18,6 @@ $(function() {
           }, 200);
       });
 
-if ( window.document.body.id === 'index' ) {
-  const scrollTop_profile = $('#top').offset().top;
-
-  $('.button-for-profile').click(function(){
-     $('html,body').animate({
-       'scrollTop':scrollTop_profile
-     },500);
-    });
-
-
-const scrollTop_works = $('#works').offset().top;
-
-$('.button-for-works').click(function(){
-   $('html,body').animate({
-     'scrollTop':scrollTop_works
-   },500);
-  });
-
-  const scrollTop_services = $('#services').offset().top;
-
-  $('.button-for-services').click(function(){
-     $('html,body').animate({
-       'scrollTop':scrollTop_services
-     },500);
-    });
-
-
-
-  const scrollTop_contact = $('#contact').offset().top;
-
-  $('.button-for-contact').click(function(){
-     $('html,body').animate({
-       'scrollTop':scrollTop_contact
-     },500);
-    });
-
-  }
-
-
-
-        const scrollTop_works_2 = $('#works-2').offset().top;
-
-        $('.button-for-works-2').click(function(){
-           $('html,body').animate({
-             'scrollTop':scrollTop_works_2
-           },500);
-          });
-
-        const scrollTop_contact_2 = $('#contact-2').offset().top;
-
-        $('.button-for-contact-2').click(function(){
-           $('html,body').animate({
-             'scrollTop':scrollTop_contact_2
-           },500);
-          });
-
-
-
 
 
 
