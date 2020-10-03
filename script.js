33b969ecac2e28251dadd4047cfac1bdfd338233
skakeru <49814523+skakeru@users.noreply.github.com>
2020-10-03 22:28:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,78 @@
 $(function() {
 
-  if ($('.sp-menu').css('display') == 'block') {
-    $(".hamburger").click(
-      function() {
-        $(".sp-menu").css('background-color','black');
-         });
+  $(".btn-gnavi").on("click", function(){
+          // ハンバーガーメニューの位置を設定
+          var rightVal = 0;
+          if($(this).hasClass("open")) {
+              // 位置を移動させメニューを開いた状態にする
+              rightVal = -300;
+              // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+              $(this).removeClass("open");
+          } else {
+              // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+              $(this).addClass("open");
+          }
 
-
-    $(".hamburger").click(
-      function() {
-        $('.hamburger').children('img').attr('src', './images/hamburger.png');
-        });
-} else {
-  $(".hamburger").click(
-    function() {
-      $(".sp-menu").css('display','block');
-       });
-
-
-  $(".hamburger").click(
-    function() {
-      $('.hamburger').children('img').attr('src', './images/peke.png');
+          $("#global-navi").stop().animate({
+              right: rightVal
+          }, 200);
       });
-}
+
+if ( window.document.body.id === 'index' ) {
+  const scrollTop_profile = $('#top').offset().top;
+
+  $('.button-for-profile').click(function(){
+     $('html,body').animate({
+       'scrollTop':scrollTop_profile
+     },500);
+    });
+
+
+const scrollTop_works = $('#works').offset().top;
+
+$('.button-for-works').click(function(){
+   $('html,body').animate({
+     'scrollTop':scrollTop_works
+   },500);
+  });
+
+  const scrollTop_services = $('#services').offset().top;
+
+  $('.button-for-services').click(function(){
+     $('html,body').animate({
+       'scrollTop':scrollTop_services
+     },500);
+    });
+
+
+
+  const scrollTop_contact = $('#contact').offset().top;
+
+  $('.button-for-contact').click(function(){
+     $('html,body').animate({
+       'scrollTop':scrollTop_contact
+     },500);
+    });
+
+  }
+
+
+
+        const scrollTop_works_2 = $('#works-2').offset().top;
+
+        $('.button-for-works-2').click(function(){
+           $('html,body').animate({
+             'scrollTop':scrollTop_works_2
+           },500);
+          });
+
+        const scrollTop_contact_2 = $('#contact-2').offset().top;
+
+        $('.button-for-contact-2').click(function(){
+           $('html,body').animate({
+             'scrollTop':scrollTop_contact_2
+           },500);
+          });
 
 
 
